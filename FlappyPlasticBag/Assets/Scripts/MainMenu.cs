af2c84635c6466f25977174c230891142fa5a9b1
Phan Quoc Thang <thangphanquoc2407@gmail.com>
2019-11-30 01:00:58 +0700

    Main Menu

---
+++ FlappyPlasticBag/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Endless()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Survival()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
